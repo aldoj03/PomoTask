67fe7975e24b902f8df3d050fb32f7797b885590
Aldo <aldoj03@gmail.com>
2019-11-11 22:20:28 -0400

    v1

--- app.js
+++ app.js
@@ -1,66 +1,87 @@
 "use strict";
 
 window.onload = () => {
-    document.getElementById("abort").disabled = true;
-    document.getElementById("abort").style.display = "none";
-  var stetings;
-  var ini;
-  let flag;
+  var stetings = {
+    Focustime: 0,
+    Relaxtime: 0,
+    Intervals: 0,
+    Longrelaxtime: 0
+  };
+  var minInitial;
+  var focus;
+  var relax;
+  var intervals;
+  var longrelax;
+  var seconds;
   var min;
+  var status = "focus";
   var play = false;
-  var seg = document.querySelector(".outer-seconds");
+  var stage = 0;
+  var segElement = document.querySelector(".outer-seconds");
   document.getElementById("start").addEventListener("click", () => {
     start();
   });
-  document.getElementById("abort").addEventListener("click", () => {
-      abort();
-    });
-    document.getElementById("stetings").addEventListener("click", () => {
-        showStetingsForm();
-    });
-    var submitbutton;
+  document.getElementById("stetings").addEventListener("click", () => {
+    showStetingsForm();
+  });
+  var stageIcons = document.querySelector("#progress ul");
+  var submitbutton;
+
+  //* CONFIGURA LAS VARIABLES/
   function init() {
-    
-    ini = (stetings.Focustime != 'undefined') ? stetings.Focustime :  25;
-    flag = 0;
-    renderMin(ini);
+    stageIcons.children[0].style.opacity = 1;
+    focus = stetings.Focustime;
+    relax = stetings.Relaxtime;
+    intervals = stetings.Intervals;
+    longrelax = stetings.Longrelaxtime;
+
+    calculateStage();
+
+    seconds = 0;
     play = true;
   }
 
-  function abort() {
-    renderMin(0);
-    seg.style.strokeDashoffset = 0;
-    flag = 0;
+  function endInterval() {
+    setStage(stage);
+
+    if(stage <= intervals * 2)
+    {
+      calculateStage();
+    }
+    segElement.style.strokeDashoffset = 0;
+    seconds = 0;
     play = false;
-    document.getElementById("stetings").disabled = false;
-    document.getElementById("stetings").style.display = "block";
-    document.getElementById("start").disabled = false;
-    document.getElementById("start").style.display = "block";
+    show([document.getElementById("start")]);
   }
 
   function start() {
     init();
     var tmp = setInterval(() => {
       if (play == true) {
-        console.log(flag);
-        seg.style.strokeDashoffset -= 15.7;
-        flag++;
-        if (flag == 60) {
-          ini--;
-          min.innerHTML = ini;
-          flag = 0;
+        segElement.style.strokeDashoffset -= 15.7;
+        seconds++;
+        if (seconds == 60) {
+          minInitial--;
+          min.innerHTML = minInitial;
+          seconds = 0;
         }
-        if (ini <= 0) {
-          abort();
+        if (minInitial <= 0) {
+          if(stage >= intervals * 2){
+            clearInterval(tmp);      
+          }
+          else{
+            stage++;
+            endInterval();
+          }
         }
       } else {
         clearInterval(tmp);
       }
-    }, 1000);
-    document.getElementById("stetings").disabled = true;
-    document.getElementById("stetings").style.display = "none";
-    document.getElementById("start").disabled = true;
-    document.getElementById("start").style.display = "none";
+    }, 20);
+    hide([
+      document.getElementById("stetings"),
+      document.getElementById("start")
+    ]);
   }
 
   function renderMin(value) {
@@ -68,55 +89,139 @@ window.onload = () => {
     min.innerHTML = value;
   }
 
+  //**SHOW STETINGS*/
   function showStetingsForm() {
-      submitbutton  = document.querySelector("form"); 
-     console.log(submitbutton);
-     
-    document.getElementById("start").disabled = true;
-    document.getElementById("abort").disabled = true;
-    document.getElementById("stetings").disabled = true;
-    document.getElementById("start").style.display = "none";
-    document.getElementById("abort").style.display = "none";
-    document.getElementById("stetings").style.display = "none";
-    document.getElementsByTagName("svg")[0].style.display = "none";
-    var form = document.getElementById("form-stetings");
-    form.style.opacity = 1;
-  
-    submitbutton.addEventListener("submit",(ev)=>{
+    submitbutton = document.querySelector("form");
+
+    hide([
+      document.getElementById("stetings"),
+      document.getElementById("start"),
+      document.getElementsByTagName("svg")[0]
+    ]);
+
+    show([document.getElementById("form-stetings")]);
+
+    submitbutton.addEventListener("submit", ev => {
+      if (ev.lenght) {
+        show([document.getElementById("start")]);
+      }
       setStetings(ev);
-    })
+    });
+  }
 
-}
-    function setStetings(ev) {
-        stetings = {
-             Focustime: ev.target[0].value,
-             Relaxtime:  ev.target[1].value,
-             Intervals:  ev.target[2].value,
-             Longrelaxtime:  ev.target[3].value
-        };
+  //**SET STETINGS */
+  function setStetings(ev) {
+    if (
+      ev.target[0].value > 0 &&
+      ev.target[1].value > 0 &&
+      ev.target[2].value > 0 &&
+      ev.target[3].value > 0
+    ) {
+      stetings = {
+        Focustime: ev.target[0].value,
+        Relaxtime: ev.target[1].value,
+        Intervals: ev.target[2].value,
+        Longrelaxtime: ev.target[3].value
+      };
 
-        document.getElementById("start").disabled = false;
-        document.getElementById("abort").disabled = false;
-        document.getElementById("stetings").disabled = false;
-        document.getElementById("start").style.display = "block";
-        document.getElementById("abort").style.display = "block";
-        document.getElementById("stetings").style.display = "block";
-        document.getElementsByTagName("svg")[0].style.display = "block";
-        var form = document.getElementById("form-stetings");
-        form.style.opacity = 0;
-        return stetings;
+      renderAllstages(stetings.Intervals);
+      init();
+      closeForm();
+    } else {
+      alert("ningun valor puede ser 0 o negativo");
     }
-    
-    document.getElementById("close").addEventListener("click", ()=>{
-        document.getElementById("start").disabled = false;
-        document.getElementById("abort").disabled = false;
-        document.getElementById("stetings").disabled = false;
-        document.getElementById("start").style.display = "block";
-        document.getElementById("abort").style.display = "block";
-        document.getElementById("stetings").style.display = "block";
-        document.getElementsByTagName("svg")[0].style.display = "block";
-        var form = document.getElementById("form-stetings");
-        form.style.opacity = 0;
-    })
-  
+
+    return false;
+  }
+
+  //*CLOSE STETINGS */
+  function closeForm() {
+    if (focus && relax && intervals && longrelax) {
+      show([document.getElementById("start")]);
+      hide([document.getElementById("stetings")]);
+    } else {
+      show([document.getElementById("stetings")]);
+    }
+
+    show([document.getElementsByTagName("svg")[0]]);
+    hide([document.getElementById("form-stetings")]);
+  }
+
+  document.querySelector(".close").addEventListener("click", () => {
+    closeForm();
+  });
+
+  function setStage(actual) {
+    stageIcons.children[actual].style.opacity = 1;
+    stageIcons.children[actual].style.display = "flex";
+  }
+
+  //*SHOW HIDE */
+
+  function show(element) {
+    element.forEach(item => {
+      item.style.display = "block";
+      setTimeout(() => {
+        item.style.opacity = 1;
+      }, 200);
+    });
+  }
+  function hide(element) {
+    element.forEach(item => {
+      item.style.opacity = 0;
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 200);
+    });
+  }
+
+  //*SHOW MESSAGE */
+  function showEstageMessage() {
+    var element = document.querySelector("#message");
+    element.innerHTML = status;
+  }
+
+  function renderAllstages(interv) {
+    document.querySelector("#progress").style.display = 'flex';
+    var progressBar = document.querySelector("#progress ul");
+
+    for (let index = 0; index < interv * 2 + 1; index++) {
+      var element = document.createElement("LI");
+
+      if (index % 2 != 0) {
+        element.classList.add("relax");
+        var content = document.createTextNode("◼");
+      }
+      if (index % 2 == 0) {
+        element.classList.add("focus");
+        var content = document.createTextNode("✓");
+      }
+
+      if (index == interv * 2) {
+        element.classList.add("longrelax");
+        var content = document.createTextNode("⊝");
+      }
+      element.appendChild(content);
+      progressBar.appendChild(element);
+    }
+  }
+
+  function calculateStage() {
+    if (stage % 2 == 0 || stage == 0) {
+      minInitial = focus;
+      status = "focus";
+    }
+    if (stage % 2 != 0) {
+      minInitial = relax;
+      status = "relax";
+    }
+
+    if (stage >= intervals * 2) {
+      minInitial = longrelax;
+      status = "long relax";
+    }
+
+    renderMin(minInitial);
+    showEstageMessage();
+  }
 };
